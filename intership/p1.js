952b2e522585c8394b7601c25d13f4e627da7635
kartikambig149 <149179934+kartikambig149@users.noreply.github.com>
2023-11-07 19:17:00 +0530

    Update p1.js

--- intership/p1.js
+++ intership/p1.js
@@ -1,4 +1,4 @@
-// JavaScript for dynamic form handling, preview, and download
+
 document.getElementById('resume-form').addEventListener('submit', function (e) {
     e.preventDefault();
     updatePreview();
@@ -6,9 +6,8 @@ document.getElementById('resume-form').addEventListener('submit', function (e) {
   });
   
   function updatePreview() {
-    // Implement code to update the resume preview based on form inputs
+
     const preview = document.getElementById('preview');
-    // Example: Display the entered name in the preview
     const nameInput = document.querySelector('input[placeholder="Full Name"]');
     const name = nameInput.value || 'Your Name';
     preview.innerHTML = `<h2>${name}</h2>`;
